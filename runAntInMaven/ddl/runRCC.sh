a97f268836b0458b66d46c3402ebaba1609f95ad
zhishan <zhishan@yahoo-inc.com>
2014-02-21 11:48:25 +0000

    add ddl for hadoop rcc

---
+++ runAntInMaven/ddl/runRCC.sh
@@ -0,0 +1,7 @@
+$HADOOP_INSTALL/bin/rcc --language java *.jr
+>tree com
+com
+`-- mr 
+    `-- uda
+        `-- ft
+            `-- *.java
---
+++ runAntInMaven/run_ant_in_maven/src/test/java/com/myhp/runAntInMaven/AppTest.java
@@ -0,0 +1,14 @@
+package com.myhp.runAntInMaven;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testMain() {
+        String[] arg = {"how to test static method in java"};
+        App.main(arg);
+    }
+}
